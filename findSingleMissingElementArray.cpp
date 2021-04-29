04001ffd89f42f74f6d09109131aa9077a117203
AarthiNataraja <58328440+AarthiNataraja@users.noreply.github.com>
2021-04-29 12:33:04 +0530

    Update findSingleMissingElementArray.cpp

--- findSingleMissingElementArray.cpp
+++ findSingleMissingElementArray.cpp
@@ -40,17 +40,18 @@ int main()
     cout << "Missing Element: " << (sum - total) << endl;
     
     //using hash table - normal array.
-    int arr2[size] = {1,2,3,5,6,7,8,9,10,11};
+    int arr2[size] = {6,7,8,10,11,12,14,15,16,17};
     
     int size2 = arr2[size-1];
     int hash[size2] = {0};
+    int l = arr2[0];
     
-    for(int k = 0; k < size2 - 1; k++)
+    for(int k = 0; k < size; k++)
     {
         hash[arr2[k]]++;
     }
     
-    for(int l = 1; l < size2; l++)
+    for(; l < size2; l++)
     {
         if(hash[l] == 0)
         {
